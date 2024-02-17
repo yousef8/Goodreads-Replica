cad9d671b2237e484f297b1a33031df6002e7095
aya maher <79773094+ayamaher205@users.noreply.github.com>
2024-02-17 23:25:56 +0200

    Update validate.js

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -3,10 +3,10 @@ import Joi from "joi";
 
 const validatorLogin = (req, res, next) => {
   const loginSchema = Joi.object({
-    username: Joi.string().pattern("/^[a-z0-9_-]{3,15}$/.test(v)"),
+    username: Joi.string().pattern("/^[a-z0-9_-]{3,15}$/.test(v)").required(),
     password: Joi.string().pattern(
       "/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/",
-    ),
+    ).required(),
   });
   const validationResult = Joi.validate(req.body, loginSchema);
     if ( !validationResult.error ) { return next(); }
