c110f858779071f2524048c87fca37cda11d494f
Yousef Elsayed <yousef4511@outlook.com>
2024-02-17 23:52:49 +0200

    Feat: Add register API

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,58 @@
-function register(req, res) {
-  res.status(501).json({ message: "To-Do" });
+import Joi from "joi";
+import User from "../models/user.js";
+import asyncWrapper from "../utils/asyncWrapper.js";
+
+async function register(req, res, next) {
+  const schema = Joi.object({
+    firstName: Joi.string()
+      .trim()
+      .pattern(/^[a-zA-Z]+$/)
+      .min(3)
+      .max(15)
+      .required(),
+    lastName: Joi.string()
+      .trim()
+      .pattern(/^[a-zA-Z]+$/)
+      .min(3)
+      .max(15)
+      .required(),
+    username: Joi.string()
+      .trim()
+      .pattern(/^[a-z0-9_-]+$/, "username")
+      .min(3)
+      .max(15)
+      .required(),
+    email: Joi.string().trim().email().required(),
+    password: Joi.string()
+      .trim()
+      .pattern(
+        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/,
+        "password",
+      )
+      .required(),
+    isAdmin: Joi.boolean(),
+  });
+
+  const [joiError, validRequest] = await asyncWrapper(
+    schema.validateAsync(req.body),
+  );
+
+  if (joiError) {
+    next(joiError);
+    return;
+  }
+
+  const [mongooseError, user] = await asyncWrapper(
+    new User(validRequest).save(),
+  );
+
+  if (mongooseError) {
+    console.log(mongooseError);
+    next(mongooseError);
+    return;
+  }
+
+  res.status(201).json(user);
 }
 
 export default { register };
