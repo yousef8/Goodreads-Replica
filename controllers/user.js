4db439f2d99e337f2d14f2464cf5be3abe946051
Yousef Elsayed <yousef4511@outlook.com>
2024-02-18 17:36:22 +0200

    Refactor: Make separate middleware for register schema validation

--- controllers/user.js
+++ controllers/user.js
@@ -1,49 +1,9 @@
-import Joi from "joi";
 import User from "../models/user.js";
 import asyncWrapper from "../utils/asyncWrapper.js";
 
 async function register(req, res, next) {
-  const schema = Joi.object({
-    firstName: Joi.string()
-      .trim()
-      .pattern(/^[a-zA-Z]+$/)
-      .min(3)
-      .max(15)
-      .required(),
-    lastName: Joi.string()
-      .trim()
-      .pattern(/^[a-zA-Z]+$/)
-      .min(3)
-      .max(15)
-      .required(),
-    username: Joi.string()
-      .trim()
-      .pattern(/^[a-z0-9_-]+$/, "username")
-      .min(3)
-      .max(15)
-      .required(),
-    email: Joi.string().trim().email().required(),
-    password: Joi.string()
-      .trim()
-      .pattern(
-        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/,
-        "password",
-      )
-      .required(),
-    isAdmin: Joi.boolean(),
-  });
-
-  const [joiError, validRequest] = await asyncWrapper(
-    schema.validateAsync(req.body),
-  );
-
-  if (joiError) {
-    next(joiError);
-    return;
-  }
-
   const [mongooseError, user] = await asyncWrapper(
-    new User(validRequest).save(),
+    new User(req.validReq).save(),
   );
 
   if (mongooseError) {
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 import express from "express";
 import userCtrler from "../controllers/user.js";
+import validateRegisterReq from "../middlewares/validateRegisterReq.js";
 
 const router = express.Router();
 
-router.post("/user", userCtrler.register);
+router.post("/user", validateRegisterReq, userCtrler.register);
 
 export default router;
