091650540b78d994f6d347c649cac65712ebe056
Yousef Elsayed <yousef4511@outlook.com>
2024-02-23 01:10:51 +0200

    Refactor: Split error handling middleware

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import routes from "./routes/index.js";
-import CustomError from "./errors/customError.js";
+import errorHandler from "./middlewares/errorHandler.js";
 
 const port = process.env.PORT || 5000;
 const corsOptions = {
@@ -19,59 +19,7 @@ app.use(cors(corsOptions));
 
 app.use(routes);
 
-app.use((err, req, res, next) => {
-  // Handle Joi Schema Validation Error
-  if (err.isJoi) {
-    res.status(400).json(
-      err.details.reduce((message, error) => {
-        message[error.context.key] = error.message;
-        return message;
-      }, {}),
-    );
-    return;
-  }
-
-  // Handle Mongoose Validation Error
-  if (err.name === "ValidationError") {
-    const errors = Object.entries(err.errors);
-
-    res.status(400).json(
-      errors.reduce((message, [key, value]) => {
-        message[key] = value.message;
-        return message;
-      }, {}),
-    );
-    return;
-  }
-
-  // Handle express.json() parsing Error
-  if (err.type === "entity.parse.failed" && err instanceof SyntaxError) {
-    res.status(400).json({
-      message: err.message,
-    });
-    return;
-  }
-
-  // Handle Our custome error
-  if (err instanceof CustomError) {
-    res.status(err.status).json({ message: err.message });
-    return;
-  }
-
-  // Handle Mongoose duplicate key error
-  if (
-    err.name === "MongoServerError" &&
-    (err.code === 11000 || err.code === 11001)
-  ) {
-    res
-      .status(400)
-      .json({ message: `Duplication Error: ${err.keyValue.name}` });
-    return;
-  }
-
-  console.log("UnHandled Error", err);
-  res.status(500).json({ message: `Internal Server Error: ${err.message}` });
-});
+app.use(errorHandler);
 
 mongoose.connect("mongodb://127.0.0.1:27017/GoodreadsApp").then(() => {
   app.listen(port, () => {
---
+++ middlewares/errorHandler.js
@@ -0,0 +1,57 @@
+import CustomError from "../errors/customError.js";
+
+async function errorHandler(err, req, res, next) {
+  // Handle Joi Schema Validation Error
+  if (err.isJoi) {
+    res.status(400).json(
+      err.details.reduce((message, error) => {
+        message[error.context.key] = error.message;
+        return message;
+      }, {}),
+    );
+    return;
+  }
+
+  // Handle Mongoose Validation Error
+  if (err.name === "ValidationError") {
+    const errors = Object.entries(err.errors);
+
+    res.status(400).json(
+      errors.reduce((message, [key, value]) => {
+        message[key] = value.message;
+        return message;
+      }, {}),
+    );
+    return;
+  }
+
+  // Handle express.json() parsing Error
+  if (err.type === "entity.parse.failed" && err instanceof SyntaxError) {
+    res.status(400).json({
+      message: err.message,
+    });
+    return;
+  }
+
+  // Handle Our custome error
+  if (err instanceof CustomError) {
+    res.status(err.status).json({ message: err.message });
+    return;
+  }
+
+  // Handle Mongoose duplicate key error
+  if (
+    err.name === "MongoServerError" &&
+    (err.code === 11000 || err.code === 11001)
+  ) {
+    res
+      .status(400)
+      .json({ message: `Duplication Error: ${err.keyValue.name}` });
+    return;
+  }
+
+  console.log("UnHandled Error", err);
+  res.status(500).json({ message: `Internal Server Error: ${err.message}` });
+}
+
+export default errorHandler;
