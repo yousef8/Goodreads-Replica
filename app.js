3cc3fec4fa0642d3098a73940462aa8671940b29
Yousef Elsayed <yousef4511@outlook.com>
2024-02-29 21:50:22 +0200

    Feat: Add db url to .env file

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const corsOptions = {
 
 const app = express();
 
-app.use(cors(corsOptions)); 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/images", express.static("images"));
 
@@ -22,8 +22,10 @@ app.use(routes);
 
 app.use(errorHandler);
 
-mongoose.connect("mongodb://127.0.0.1:27017/GoodreadsApp").then(() => {
+const LOCAL_DB = "mongodb://127.0.0.1:27017/GoodreadsApp";
+mongoose.connect(process.env.DB_URL || LOCAL_DB).then(() => {
   app.listen(port, () => {
+    console.log(`DB connected to ${process.env.DB_URL || LOCAL_DB}`);
     console.log(`Server is listening on port ${port}`);
   });
 });
